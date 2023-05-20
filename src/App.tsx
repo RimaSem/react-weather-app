5c2d838f3271570e6597242e7844363ec5a5aeed
RimaSem <rima.semetaite@gmail.com>
2023-05-20 21:03:57 +0300

    Testing env 2

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const App: React.FC = () => {
 
   const API_KEY = import.meta.env.VITE_API_KEY;
   const errorRef = useRef<HTMLDivElement>(null);
+  console.log(import.meta.env.VITE_API_KEY);
 
   useEffect(() => {
     axios
